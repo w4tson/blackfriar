1a6876c7ff4da0aa9d8a1fe84ef212024f0dcfb8
Paul Watson <paul.watson@pdwtech.com>
2016-04-17 19:48:49 +0100

    Remove hard coded data

--- blackfriar-services/src/main/java/com/blackfriar/BeerServiceImpl.java
+++ blackfriar-services/src/main/java/com/blackfriar/BeerServiceImpl.java
@@ -4,25 +4,15 @@ import com.blackfriar.domain.Beer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by paulwatson on 06/03/2016.
  */
 @Service
 public class BeerServiceImpl implements BeerService {
-
-    private static List<Beer> beers = new ArrayList<Beer>();
-
-    static {
-
-        Beer anchorSteam = new Beer();
-        anchorSteam.setId(1L);
-        anchorSteam.setName("Anchor Steam");
-        anchorSteam.setPrice(10L);
-
-        beers.add(anchorSteam);
-    }
 
     @Autowired
     BeerRepository beerRepository;
@@ -37,11 +27,6 @@ public class BeerServiceImpl implements BeerService {
 
     @Override
     public Optional<Beer> getById(Long id) {
-        List<Beer> beers = new ArrayList<>();
-        beerRepository.findAll().forEach(beers::add);
-        return beers.stream()
-                .filter(b -> Objects.equals(b.getId(), id))
-                .findAny();
-
+        return Optional.ofNullable(beerRepository.findOne(id));
     }
 }
