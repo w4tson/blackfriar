6f2f26e2656d98c2507fce6c703dd7ea2f5a806e
Paul Watson <paul.watson@pdwtech.com>
2017-03-03 14:17:33 +0000

    Transaction annotation
    Need to add Exception.class because by default spring only rollsback on runtime exceptions

--- blackfriar-services/src/main/java/com/blackfriar/BeerService.java
+++ blackfriar-services/src/main/java/com/blackfriar/BeerService.java
@@ -1,6 +1,7 @@
 package com.blackfriar;
 
 import com.blackfriar.domain.Beer;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 /**
  * Created by paulwatson on 06/03/2016.
  */
+@Transactional(rollbackFor = Exception.class) //By default spring only rollsback on runtime exceptions
 public interface BeerService {
 
     List<Beer> getAllBeers();
